0a62ae7d6a2dff0effa19441961f1fcc69cd8172
Pattnatt <jittapat007@gmail.com>
2018-06-14 08:43:55 +0300

    Add Debug Mode to PlayerEnergy for testing

--- Volatile Space - Movement Demo/Assets/Scripts/PlayerShipEnergy.cs
+++ Volatile Space - Movement Demo/Assets/Scripts/PlayerShipEnergy.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShipEnergy : MonoBehaviour {
+    public bool isDebug = false;
+
     public int maxEnergy = 10;
     public int maxShield = 5;
     public int maxWeapon = 5;
@@ -11,6 +13,10 @@ public class PlayerShipEnergy : MonoBehaviour {
     public int currentShield { get; private set; }
     public int currentWeapon { get; private set; }
     public int currentMove { get; private set; }
+
+    public int startShield;
+    public int startWeapon;
+    public int startMove;
 
     void Start () {
         SetInitialValue();
@@ -25,6 +31,18 @@ public class PlayerShipEnergy : MonoBehaviour {
         currentShield = 0;
         currentWeapon = 0;
         currentMove = 0;
+        if (isDebug) {
+            if(startShield + startWeapon + startMove > maxEnergy) {
+                currentShield = Mathf.Clamp((int)((float)startShield / ((float)(startShield + startWeapon + startMove)) * (float)maxEnergy), 0, maxShield);
+                currentWeapon = Mathf.Clamp((int)((float)startWeapon / ((float)(startShield + startWeapon + startMove)) * (float)maxEnergy), 0, maxWeapon);
+                currentMove = Mathf.Clamp(maxEnergy - (currentWeapon + currentShield), 0, maxMove);
+            }
+            else {
+                currentShield = Mathf.Clamp(startShield, 0, maxShield);
+                currentWeapon = Mathf.Clamp(startWeapon, 0, maxWeapon);
+                currentMove = Mathf.Clamp(startMove, 0, maxMove);
+            }
+        }
     }
 
     public int RemainingEnergy () {
@@ -51,11 +69,11 @@ public class PlayerShipEnergy : MonoBehaviour {
                 currentShield = Mathf.Clamp(maxEnergy - (currentMove + currentWeapon), 0, maxShield);
             }
             else if (move != currentMove) {
+                currentMove = Mathf.Clamp(move, 0, maxMove);
                 currentShield = Mathf.Clamp((int)((float)shield / ((float)(shield + weapon + move)) * (float)maxEnergy), 0, maxShield);
                 currentWeapon = Mathf.Clamp(maxEnergy - (currentShield + currentMove), 0, maxWeapon);
             }
             else {
-                currentMove = Mathf.Clamp(move, 0, maxMove);
                 currentShield = Mathf.Clamp((int)((float)shield / ((float)(shield + weapon + move)) * (float)maxEnergy), 0, maxShield);
                 currentWeapon = Mathf.Clamp((int)((float)weapon / ((float)(shield + weapon + move)) * (float)maxEnergy), 0, maxWeapon);
                 currentMove = Mathf.Clamp(maxEnergy - (currentWeapon + currentShield), 0, maxMove);
